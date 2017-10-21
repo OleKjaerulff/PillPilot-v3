c5c415cabb95c619f6d7492098a4ec6ec9b2d9ce
OleKjaerulff <okjaerulff@sund.ku.dk>
2017-10-21 16:23:31 +0200

    Succesfully tested how to assign a string value by user entry in EditText field.

--- PillPilot-v3/MainActivity.cs
+++ PillPilot-v3/MainActivity.cs
@@ -1,6 +1,8 @@
 ﻿using Android.App;
 using Android.Widget;
 using Android.OS;
+using Android.Content;
+using Android.Preferences;
 
 namespace PillPilot_v3
 {
@@ -13,6 +15,38 @@ namespace PillPilot_v3
 
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
+
+
+            EditText morgenNavn1 = FindViewById<EditText>(Resource.Id.morgenNavn1);
+            TextView labelMorgen = FindViewById<TextView>(Resource.Id.labelMorgen);
+
+            morgenNavn1
+                .TextChanged += (object sender, Android.Text.TextChangedEventArgs e) => {
+
+                    labelMorgen.Text = e.Text.ToString();
+
+            };
+
+
+
+            /*
+            string morgenNavn1val = "Ole";
+            ISharedPreferences prefs = PreferenceManager.GetDefaultSharedPreferences(this);
+            ISharedPreferencesEditor editor = prefs.Edit();
+            editor.PutString("morgenNavn1", morgenNavn1val);
+            // editor.Commit();    // applies changes synchronously on older APIs
+            editor.Apply();        // applies changes asynchronously on newer APIs
+
+
+
+            // mInt = prefs.GetInt("key_for_my_int_value", < default value >);
+            morgenNavn1val = prefs.GetString("morgenNavn1", morgenNavn1val);
+            morgenNavn1.Text = morgenNavn1val;
+            */
+
+
+
+
         }
     }
 }
