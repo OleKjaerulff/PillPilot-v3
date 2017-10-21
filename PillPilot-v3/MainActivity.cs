cc5dab48d5df9e5a0c23818a8874c0a56f974e57
OleKjaerulff <okjaerulff@sund.ku.dk>
2017-10-21 18:33:33 +0200

    Succesfully used SharedPreferences to remember userinput to morgenNavn1 EditText field between sessions.

--- PillPilot-v3/MainActivity.cs
+++ PillPilot-v3/MainActivity.cs
@@ -16,37 +16,21 @@ namespace PillPilot_v3
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
 
-
             EditText morgenNavn1 = FindViewById<EditText>(Resource.Id.morgenNavn1);
-            TextView labelMorgen = FindViewById<TextView>(Resource.Id.labelMorgen);
+            
+            ISharedPreferences prefs = PreferenceManager.GetDefaultSharedPreferences(this);
+            ISharedPreferencesEditor editor = prefs.Edit();
 
+            morgenNavn1.Text = prefs.GetString("morgenNavn1", "");
+            
             morgenNavn1
                 .TextChanged += (object sender, Android.Text.TextChangedEventArgs e) => {
 
-                    labelMorgen.Text = e.Text.ToString();
+                    editor.PutString("morgenNavn1", e.Text.ToString());
+                    // editor.Commit();    // applies changes synchronously on older APIs
+                    editor.Apply();        // applies changes asynchronously on newer APIs
 
-            };
-
-
-
-            /*
-            string morgenNavn1val = "Ole";
-            ISharedPreferences prefs = PreferenceManager.GetDefaultSharedPreferences(this);
-            ISharedPreferencesEditor editor = prefs.Edit();
-            editor.PutString("morgenNavn1", morgenNavn1val);
-            // editor.Commit();    // applies changes synchronously on older APIs
-            editor.Apply();        // applies changes asynchronously on newer APIs
-
-
-
-            // mInt = prefs.GetInt("key_for_my_int_value", < default value >);
-            morgenNavn1val = prefs.GetString("morgenNavn1", morgenNavn1val);
-            morgenNavn1.Text = morgenNavn1val;
-            */
-
-
-
-
+                };
         }
     }
 }
